9e563585105e4ac8c97ad272443b9174508c3549
Kevin Rodriguez <Kevin.rodriguez4246@gmail.com>
2022-05-04 11:20:57 -0300

    Reemplazo de inputs por funcion con condicional

--- test_ie.py
+++ test_ie.py
@@ -1,35 +1,80 @@
 
+ 
+def choice_option():
+   Option = int(input("Opcion: "))
+   if Option == 1:
+      Puntaje = 1
+      return Puntaje
+
+   elif Option == 2:
+      Puntaje = 2 
+      return Puntaje
+
+   elif Option == 3:
+      Puntaje = 3
+      return Puntaje
+
+   elif Option == 4:
+      Puntaje = 4
+      return Puntaje
+   
+   elif Option == 5:
+      Puntaje = 5
+      return Puntaje
+
+   else:
+      Puntaje = 0
+      return Puntaje
+
+
+print("""
+TEST de Inteligencia Emocional
+Responde estas 15 Afirmaciones según alguna de estas opciones
+Utilizando 1, 2, 3, 4 y 5.
+1)no estás de acuerdo para nada
+2)rara vez
+3)ocasionalmente
+4)seguido
+5)muy seguido
+
+Empecemos
+""")
+
+print("No pierdo el temperamento muy facilmente. ")
+A1 = choice_option()
+print("Me puedo relacionar con las emociones de los demás. ")
+A2 = choice_option()
+print("La gente dice que sé escuchar a los demás. ")
+A3 = choice_option()
+print("Me resulta fácil expresar mis emociones. ")
+A4 = choice_option()
+print("Me puedo adaptar a mis colegas en el trabajo. ")
+A5 = choice_option()
+print("Puedo dejar pasar ciertas cosas que me molestan fácilmente. ")
+A6 = choice_option()
+print("No me siento molesto cuando alguien me critica. ")
+A7 = choice_option()
+print("Soy muy consciente de lo que hago bien y de lo que hago mal. ")
+A8 = choice_option()
+print("No evito comunicarme con las personas para ahorrarme conflictos. ")
+A9 = choice_option()
+print("Disfruto organizar y gestionar eventos en mi casa o trabajo. ")
+A10 = choice_option()
+print("Me siento muy mal cuando veo gente sin hogar, en la calle. ")
+A11 = choice_option()
+print("Puedo controlar mi enojo todo el tiempo. ")
+A12 = choice_option()
+print("Mis amigos pueden confiar en mi. ")
+A13 = choice_option()
+print("No suelo cambiar muy seguido de estados de ánimo. ")
+A14 = choice_option()
+print("Es fácil para mi empezar una conversación. ")
+A15 = choice_option()
+
+
+resultado = A1 + A2 + A3 + A4 + A5 + A6 + A7 + A8 + A9 + A10 + A11 + A12 + A13 + A14 + A15
+
 def run():
-   print("""
-   Medidor de Inteligencia Emocional
-   Contesta estas 15 Afirmaciones según alguna de estas opciones
-   Utilizando 1, 2, 3, 4 o 5
-   1 no estás de acuerdo para nada
-   2 rara vez
-   3 ocasionalmente
-   4 seguido
-   5 muy seguido
-
-   Empecemos""")
-
-   A1 = int(input("No pierdo el temperamento muy facilmente. "))
-   A2 = int(input("Me puedo relacionar con las emociones de los demás. "))
-   A3 = int(input("La gente dice que sé escuchar a los demás. "))
-   A4 = int(input("Me resulta fácil expresar mis emociones. "))
-   A5 = int(input("Me puedo adaptar a mis colegas en el trabajo. "))
-   A6 = int(input("Puedo dejar pasar ciertas cosas que me molestan fácilmente. "))
-   A7 = int(input("No me siento molesto cuando alguien me critica. "))
-   A8 = int(input("Soy muy consciente de lo que hago bien y de lo que hago mal. "))
-   A9 = int(input("No evito comunicarme con las personas para ahorrarme conflictos. "))
-   A10 = int(input("Disfruto organizar y gestionar eventos en mi casa o trabajo. "))
-   A11 = int(input("Puedo controlar mi enojo todo el tiempo. "))
-   A12 = int(input("Me siento muy mal cuando veo gente sin hogar, en la calle. "))
-   A13 = int(input("No suelo cambiar muy seguido de estados de ánimo. "))
-   A14 = int(input("Es fácil para mi empezar una conversación. "))
-   A15 = int(input("Mis amigos pueden confiar en mi. "))
-
-   resultado = A1 + A2 + A3 + A4 + A5 + A6 + A7 + A8 + A9 + A10 + A11 + A12 + A13 + A14 + A15
-
    print("""
    Ahora sumaremos el puntaje de tus 15 respuestas
    Tu resultado es """ + str(resultado) + """
@@ -38,5 +83,6 @@ def run():
    Entre 51 y 75 tenés una inteligencia emocional alta.
    """)
 
+
 if __name__ == "__main__":
-   run() 
+  run() 
